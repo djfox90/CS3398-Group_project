8eddcb3b14d4f0303247c7f957ab22f08f5e753a
dmutka <daemon.mutka98@gmail.com>
2023-04-21 20:38:30 -0500

    Formatted to black standards

--- spotifyAPI.py
+++ spotifyAPI.py
@@ -6,88 +6,88 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-client_id = os.getenv('CLIENT_ID')
-client_secret = os.getenv('CLIENT_SECRET')
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
 
 def request_auth():
-    auth_string = client_id + ':' +client_secret
-    auth_bytes = auth_string.encode('utf-8')
-    auth_base64 = str(base64.b64encode(auth_bytes), 'utf-8')
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
 
-    url = 'https://accounts.spotify.com/api/token'
+    url = "https://accounts.spotify.com/api/token"
     headers = {
-        'Authorization': 'Basic ' + auth_base64,
-        'Content-Type': 'application/x-www-form-urlencoded'
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded",
     }
-    params = {
-        'grant_type': 'client_credentials'
-    }
+    params = {"grant_type": "client_credentials"}
     res = rq.post(url, headers=headers, data=params)
     json_res = json.loads(res.content)
-    token = json_res['access_token']
+    token = json_res["access_token"]
     return token
 
+
 def get_header(token):
-    return {'Authorization': 'Bearer ' + token}
+    return {"Authorization": "Bearer " + token}
+
 
 def search_artist(token, name):
-    url = 'https://api.spotify.com/v1/search'
+    url = "https://api.spotify.com/v1/search"
     headers = get_header(token)
-    query = f'?q={name}&type=artist&limit=1'
+    query = f"?q={name}&type=artist&limit=1"
     query_url = url + query
 
     res = rq.get(query_url, headers=headers)
-    json_res = json.loads(res.content)['artists']['items']
-    if(len(json_res) == 0):
+    json_res = json.loads(res.content)["artists"]["items"]
+    if len(json_res) == 0:
         return None
     else:
         return json_res[0]
+
 
 def search_song(token, song_title):
-    url = 'https://api.spotify.com/v1/search'
+    url = "https://api.spotify.com/v1/search"
     headers = get_header(token)
-    query = f'?q={song_title}&type=track&limit=1'
+    query = f"?q={song_title}&type=track&limit=1"
     query_url = url + query
 
     res = rq.get(query_url, headers=headers)
-    json_res = json.loads(res.content)['tracks']['items']
-    if(len(json_res) == 0):
+    json_res = json.loads(res.content)["tracks"]["items"]
+    if len(json_res) == 0:
         return None
     else:
         return json_res[0]
-    
+
+
 def search_album(token, album_title):
-    url = 'https://api.spotify.com/v1/search'
+    url = "https://api.spotify.com/v1/search"
     headers = get_header(token)
-    query = f'?q={album_title}&type=album&limit=1'
+    query = f"?q={album_title}&type=album&limit=1"
     query_url = url + query
 
     res = rq.get(query_url, headers=headers)
-    json_res = json.loads(res.content)['albums']['items']
-    if(len(json_res) == 0):
+    json_res = json.loads(res.content)["albums"]["items"]
+    if len(json_res) == 0:
         return None
     else:
         return json_res[0]
 
-def get_recomendations(token, limit = 1, artists=['None'], genres=['None'], tracks=['None']):
-    genres_string = ','.join(genres)
-    artists_string = ','.join([search_artist(token, i)['id'] for i in artists])
-    tracks_string = ','.join([search_song(token, i)['id'] for i in tracks])
 
-    url = 'https://api.spotify.com/v1/recommendations'
+def get_recommendations(
+    token, limit=1, artists=["None"], genres=["None"], tracks=["None"]
+):
+    genres_string = ",".join(genres)
+    artists_string = ",".join([search_artist(token, i)["id"] for i in artists])
+    tracks_string = ",".join([search_song(token, i)["id"] for i in tracks])
+
+    url = "https://api.spotify.com/v1/recommendations"
     headers = get_header(token)
-    query = f'?limit={limit}&market=ES&seed_artists={artists_string}&seed_genres={genres_string},country&seed_tracks={tracks_string}'
+    query = f"?limit={limit}&market=ES&seed_artists={artists_string}&seed_genres={genres_string},country&seed_tracks={tracks_string}"
     query_url = url + query
 
     res = rq.get(query_url, headers=headers)
     json_res = json.loads(res.content)
-    if(len(json_res) == 0):
+    if len(json_res) == 0:
         return None
     else:
         return json_res
-
-#print(search_artist(request_auth(), 'Michael Jackson')['id'])
-print(search_song(request_auth(), 'Thriller')['id'])
-song = get_recomendations(request_auth(), artists=['Michael Jackson'], genres=['pop', 'rap'], tracks=['gods plan'])['tracks'][0]
-print(song['name'])
-print(song['artists'][0]['name'])
